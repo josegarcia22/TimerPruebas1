59b15f3dc59dbeaba0f0e543359baeb2bc0dad27
Talio Tecnico <Talio Tecnico@P095>
2023-06-27 16:08:52 +0200

    Agregar archivos de proyecto.

---
+++ TimerPruebas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TimerPruebas.Implementations;
+
+//1
+namespace TimerPruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPublisher publi = new Publisher(new Implementations.Consola());
+            publi.StartPublishing();
+            Console.ReadLine();
+            publi.StopPublishing(); 
+
+
+        }
+    }
+}
